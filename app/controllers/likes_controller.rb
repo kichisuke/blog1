814e152fd637488f6e4cc1078457b46fb06201a8
kichisuke <supernova39108@yahoo.co.jp>
2020-09-26 22:03:19 +0900

    modify_like_func

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,17 @@ class LikesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
     @blog = Blog.find(params[:blog_id])
-    @alreadylike = Like.find_by(ip: request.remote_ip, blog_id: params[:blog_id])
+    # @alreadylike = Like.find_by(ip: request.remote_ip, blog_id: params[:blog_id])
+    @alreadylike = Like.find_by(ip: request.env["HTTP_X_FORWARDED_FOR"], blog_id: params[:blog_id])
     if @alreadylike
-      @delLike = Like.find_by(blog_id: params[:blog_id], ip: request.remote_ip)
+      #@delLike = Like.find_by(blog_id: params[:blog_id], ip: request.remote_ip)
+      @delLike = Like.find_by(blog_id: params[:blog_id], ip: request.env["HTTP_X_FORWARDED_FOR"])
       @delLike.destroy
       # redirect_back(fallback_location: root_path)
       # flash[:notice] = "You alredy liked it, thank you!"
     else
-      @like = Like.create(blog_id: params[:blog_id], ip: request.remote_ip)
+      # @like = Like.create(blog_id: params[:blog_id], ip: request.remote_ip)
+      @like = Like.create(blog_id: params[:blog_id], ip: request.env["HTTP_X_FORWARDED_FOR"])
       # redirect_back(fallback_location: root_path)
     end
   end

c0196ae6d492eb82d4ed70ce7a97242eaad9849f
kichisuke <supernova39108@yahoo.co.jp>
2020-10-03 00:06:04 +0900

    modify_modile_layout

--- app/javascript/packs/rotate.js
+++ app/javascript/packs/rotate.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
-  let id = document.getElementById('header_img').getAttribute('value');
-  let img = document.getElementById('header_img');
-  let mobile_img = document.getElementById('mobile_header_img');
-  let sub_img = document.getElementById('sub_header_img');
-  console.log(id);
-      switch(id){
-        case '0':
-          img.style.transform = 'rotate(0deg)';
-          mobile_img.style.transform = 'rotate(0deg)';
-          sub_img.style.transform = 'rotate(0deg)';
-          break;
-        case '1':
-          img.style.transform = 'rotate(90deg)';
-          mobile_img.style.transform = 'rotate(90deg)';
-          sub_img.style.transform = 'rotate(90deg)';
-          break;
-        case '2':
-          img.style.transform = 'rotate(180deg)';
-          mobile_img.style.transform = 'rotate(180deg)';
-          sub_img.style.transform = 'rotate(180deg)';
-          console.log(img);
-          break;
-        case '3':
-          img.style.transform = 'rotate(270deg)';
-          mobile_img.style.transform = 'rotate(270deg)';
-          sub_img.style.transform = 'rotate(270deg)';
-          break;
-    }
-}
-);
+// $(document).ready(function(){
+//   let id = document.getElementById('header_img').getAttribute('value');
+//   let img = document.getElementById('header_img');
+//   let mobile_img = document.getElementById('mobile_header_img');
+//   let sub_img = document.getElementById('sub_header_img');
+//   console.log(id);
+//       switch(id){
+//         case '0':
+//           img.style.transform = 'rotate(0deg)';
+//           mobile_img.style.transform = 'rotate(0deg)';
+//           sub_img.style.transform = 'rotate(0deg)';
+//           break;
+//         case '1':
+//           img.style.transform = 'rotate(90deg)';
+//           mobile_img.style.transform = 'rotate(90deg)';
+//           sub_img.style.transform = 'rotate(90deg)';
+//           break;
+//         case '2':
+//           img.style.transform = 'rotate(180deg)';
+//           mobile_img.style.transform = 'rotate(180deg)';
+//           sub_img.style.transform = 'rotate(180deg)';
+//           console.log(img);
+//           break;
+//         case '3':
+//           img.style.transform = 'rotate(270deg)';
+//           mobile_img.style.transform = 'rotate(270deg)';
+//           sub_img.style.transform = 'rotate(270deg)';
+//           break;
+//     }
+// }
+// );

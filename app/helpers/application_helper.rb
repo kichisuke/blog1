e767e523e71a577ec776169123767613a5bce310
kichisuke <supernova39108@yahoo.co.jp>
2020-10-06 11:20:09 +0900

    add_twitter_card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'AnyTopicsJapan',
+      title: 'Toppage',
+      reverse: true,
+      separator: '|',
+      description: 'Need information from Japan? AnyTopicsJapan shares realtime info from Japan.',
+      keywords: 'Japan, Japanese, Japanese blog',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: image_url("logo.png"),
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description, 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('logo.png'),
+        locale: 'en_US',
+      },
+      twitter: {
+        card: 'summary',
+        site: '@AnyTopicsJapan',
+        image: image_url('logo.png'),
+        width: 100,
+        height: 100
+      }
+    }
+  end
 end
